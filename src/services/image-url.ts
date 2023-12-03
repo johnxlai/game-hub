098d1fc47af902b1218bd589c900a326f7f9b96e
John Lai <lai.john.py@gmail.com>
2023-12-03 12:34:32 -0500

    Add sorting

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
 const getCroppedImageUrl = (url: string) => {
   // example
   // https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+  if (!url) return '';
 
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
