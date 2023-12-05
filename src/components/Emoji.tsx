ad1078b241bdb196e1253f1e750009939fd242b8
John Lai <lai.john.py@gmail.com>
2023-12-05 12:24:50 -0500

    Fix imageProps bug:

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,7 +1,7 @@
 import bullsEye from '../assets/bulls-eye.webp';
 import thumbsUp from '../assets/thumbs-up.webp';
 import meh from '../assets/meh.webp';
-import { Image } from '@chakra-ui/react';
+import { Image, ImageProps } from '@chakra-ui/react';
 
 interface Props {
   rating: number;
@@ -10,7 +10,7 @@ interface Props {
 const Emoji = ({ rating }: Props) => {
   if (rating < 3) return null;
 
-  const emojiMap: { [key: number]: ImagesProps } = {
+  const emojiMap: { [key: number]: ImageProps } = {
     3: { src: meh, alt: 'meh', boxSize: '25px' },
     4: { src: thumbsUp, alt: 'recommneded', boxSize: '25px' },
     5: { src: bullsEye, alt: 'exceptional', boxSize: '35px' },
