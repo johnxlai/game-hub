81631c4487b8c7b6b6e2d7399cfc2b10698adeb3
John Lai <lai.john.py@gmail.com>
2023-12-05 12:02:49 -0500

    Change genre to static data

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -21,7 +21,7 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
-          {game.name} <Emoji rating={game.rating_top} marginTop={1} />
+          {game.name} <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
     </Card>
