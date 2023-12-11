6bf3a05047eb69cf85b2b21f603a4cfceede42d4
John Lai <lai.john.py@gmail.com>
2023-12-10 23:46:58 -0500

    Add static data for initial loading and add staleTime

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -12,6 +12,8 @@ const useGenres = () =>
   useQuery({
     queryKey: ['genres'],
     queryFn: () => apiClient.get('/genres').then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24hr
+    initialData: { count: genres.length, results: genres },
   });
 
 export default useGenres;
