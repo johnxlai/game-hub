b20080b4982f265cb406198bccb8797ebffc6b74
John Lai <lai.john.py@gmail.com>
2023-12-12 00:15:49 -0500

    Fix useGenre hooks, ts error

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import genres from '../data/genres';
 import apiClient from '../services/api-client';
+import { FetchResponse } from './useData';
 
 export interface Genre {
   id: number;
@@ -11,7 +12,8 @@ export interface Genre {
 const useGenres = () =>
   useQuery({
     queryKey: ['genres'],
-    queryFn: () => apiClient.get('/genres').then((res) => res.data),
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>('/genres').then((res) => res.data),
     staleTime: 24 * 60 * 60 * 1000, //24hr
     initialData: { count: genres.length, results: genres },
   });
