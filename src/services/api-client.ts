ea3394d48382434cb107320e39a53dd6bbc6e7ff
John Lai <lai.john.py@gmail.com>
2023-12-13 01:14:07 -0500

    Refactor fetch game with react query, remove useData hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
 export default axios.create({
-  baseURL:'https://api.rawg.io/api',
+  baseURL: 'https://api.rawg.io/api',
   params: {
-    key:'23e2e113c8e34c1f8b9d51d01b9dd240'
-  }
-})
+    key: '23e2e113c8e34c1f8b9d51d01b9dd240',
+  },
+});
